2badf896072f13a6b34ec9d5bdcba2eb087f8558
Moses A. Samuel <mosamuel98@gmail.com>
2019-08-18 12:13:24 -0700

    add a Home component

---
+++ src/components/Home.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Home() {
+    return (
+        <div>
+            <h1>Avengers Database</h1>
+            <h4>Danger! This is classified intormation!</h4>
+        </div>
+    )
+}
