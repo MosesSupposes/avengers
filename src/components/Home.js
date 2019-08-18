3b49eed716b9cb20915c3fd987e1fef94ebdfe1a
Moses A. Samuel <mosamuel98@gmail.com>
2019-08-18 13:15:36 -0700

    Link to the AvengersList from the Home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Home() {
     return (
         <div>
             <h1>Avengers Database</h1>
             <h4>Danger! This is classified intormation!</h4>
+            <Link to="/avengers">Enter</Link>
         </div>
     )
 }
