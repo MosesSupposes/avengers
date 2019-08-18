54f15897206649cd6b447b3eff7c3118c8c58ad6
Moses A. Samuel <mosamuel98@gmail.com>
2019-08-18 12:26:29 -0700

    render a list of avengers

--- src/components/AvengersList.js
+++ src/components/AvengersList.js
@@ -1,9 +1,19 @@
 import React from 'react'
 
+import avengers from '../data'
+
 export default function AvengersList() {
     return (
-        <div>
-            <h1>Avengers List Component</h1>
+        <div className="characters-list-wrapper">
+            {avengers.map(function renderAvengers(avenger) {
+                return (
+                    <div className="character-card" key={avenger.id}>
+                        <img src={avenger.thumbnail}  alt={avenger.name}/>
+                        <h2>{avenger.name}</h2>
+                        <p>({avenger.nickname})</p>
+                    </div>
+                )
+            })}
         </div>
     )
 }
