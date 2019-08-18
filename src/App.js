d345fcee88a5da2ee8dac503c25a6615a0570b4e
Moses A. Samuel <mosamuel98@gmail.com>
2019-08-18 12:26:24 -0700

    pull in avengers' data to state

--- src/App.js
+++ src/App.js
@@ -4,9 +4,18 @@ import { Route } from 'react-router-dom'
 import Home from './components/Home'
 import AvengersList from './components/AvengersList'
 
+import avengers from './data'
+
 import './App.css'
 
 export default class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      avengers: avengers
+    }
+  }
+  
   render() {
     return (
       <div className="App">
