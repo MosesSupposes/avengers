90b35509355b594dfce01f11e399460b4fe261dc
Moses A. Samuel <mosamuel98@gmail.com>
2019-08-18 12:13:58 -0700

    render two routes to the Home and AvengersList components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react'
+import { Route } from 'react-router-dom'
+
+import Home from './components/Home'
+import AvengersList from './components/AvengersList'
+
 import './App.css'
 
 export default function App() {
   return (
     <div className="App">
-      Avengers
+      <Route exact path="/" component={Home} />
+      <Route path="/avengers" component={AvengersList} />
     </div>
   )
 }
