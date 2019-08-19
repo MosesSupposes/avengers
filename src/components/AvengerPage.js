45599c06c935bae77caa99a30c21c7e34d64e3d7
Moses A. Samuel <mosamuel98@gmail.com>
2019-08-18 18:37:46 -0700

    add nested routes for more details inside AvengerPages

--- src/components/AvengerPage.js
+++ src/components/AvengerPage.js
@@ -1,5 +1,8 @@
 import React from 'react'
+import { Route, Link } from 'react-router-dom'
 
+import AvengerDetails from './AvengerDetails'
+import AvengerMovies from './AvengerMovies'
 
 export default function AvengerPage(props) {
     const id = props.match.params.id
@@ -15,8 +18,18 @@ export default function AvengerPage(props) {
             <div className="character-info-wrapper">
                 <h1>{avenger.name}</h1>
                 <h4>({avenger.nickname})</h4>
-                <p>{avenger.description}</p>
+                
             </div> 
+            <Link to={`/avengers/${id}/details`}>Avenger Details</Link>
+            <Link to={`/avengers/${id}/movies`}>Movies</Link>
+            <Route 
+                path="/avengers/:id/details" 
+                render={props => <AvengerDetails details={avenger.description} />}
+            />
+            <Route 
+                path="/avengers/:id/movies"
+                render={props => <AvengerMovies movies={avenger.movies} />}
+            />
         </div>
     )
 }
